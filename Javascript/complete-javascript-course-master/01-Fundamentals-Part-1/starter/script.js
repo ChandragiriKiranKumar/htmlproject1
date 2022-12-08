98f9c8e413e8fe6d18f641bfacf6d439700dd163
ChandragiriKiranKumar <kiran.btech93@gmail.com>
2022-12-08 10:54:54 +0530

    basic

---
+++ Javascript/complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,36 @@
+let js = "amazing";
+console.log(40 + 8 + 23 - 10);
+console.log("Jonas"); //value
+console.log(23);
+
+let firstName = "Jonas";
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let jonas_matilda = "JM";
+let $function = 20;
+
+let person = "jonas";
+let PI = 3.1415;
+
+let myFirstJob = "Coder";
+let myCurrentJob = "Teacher";
+
+let job1 = "Programmer";
+let job2 = "teacher";
+
+console.log(myFirstJob);
+
+const marks_mass = 78;
+const marks_height = 1.69;
+const john_mass = 92;
+const john_height = 1.95;
+
+const marks_bmi = marks_mass / marks_height ** 2;
+
+const john_bmi = john_mass / john_height ** 2;
+
+var markHigherBMI = marks_bmi > john_bmi;
+
+console.log(marks_bmi, john_bmi, markHigherBMI);
---
+++ README.md
@@ -0,0 +1,2 @@
+"# sangeethamobiles" 
+"# sangeethamobiles" 
